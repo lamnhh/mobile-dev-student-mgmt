5f0f6e05aece23c9c8256aaa7b014f264b9a15ad
Lam Nguyen <lam.nguyen.hy.hoai@gmail.com>
2019-12-11 08:27:49 +0700

    Added comments

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const db = require("./db");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+/**
+ * GET /student?count=#{STUDENT_COUNT}
+ * Return STUDENT_COUNT students with smallest mssv.
+ * Return all students if STUDENT_COUNT = -1
+ */
 app.get("/student", (req, res) => {
   const count = Number(req.query.count);
   if (isNaN(count)) {
@@ -22,6 +27,14 @@ app.get("/student", (req, res) => {
     });
 });
 
+/**
+ * POST /student
+ * {
+ *    "mssv": "1712932",
+ *    "hoten": "Nguyen Hy Hoai Lam"
+ * }
+ * Insert a new student into database.
+ */
 app.post("/student", (req, res) => {
   const { mssv, hoten } = req.body;
   db.insertStudent(mssv, hoten)
@@ -33,6 +46,10 @@ app.post("/student", (req, res) => {
     });
 });
 
+/**
+ * DELETE /student/#{MSSV}
+ * Delete a student from database, given their mssv
+ */
 app.delete("/student/:mssv", (req, res) => {
   const mssv = req.params.mssv;
   db.deleteStudent(mssv)
@@ -44,6 +61,13 @@ app.delete("/student/:mssv", (req, res) => {
     });
 });
 
+/**
+ * PUT /student/#{MSSV}
+ * {
+ *    "hoten": "Nguyen Hy Hoai Lam 2"
+ * }
+ * Edit hoten of a student, given their mssv
+ */
 app.put("/student/:mssv", (req, res) => {
   const mssv = req.params.mssv;
   const hoten = req.body.hoten;
